3908d29d2e9f31131611558982184ad9d4c2f119
Vishakha-Jain <codevishakha192@gmail.com>
2023-07-11 13:04:54 +0530

    merge other

--- Employee_Management_System/settings.py
+++ Employee_Management_System/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-8g(xio@y+$*^!8g-%40me(pp(fwgsev_@40v^&51sg9bv$7l80
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app','.now.sh']
 
 
 # Application definition
